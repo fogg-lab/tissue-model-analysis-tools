2bab93b618f5e5ee3314e08fdda065804c4f56b4
cjc77 <carsonjamescook@gmail.com>
2022-03-30 17:18:33 -0700

    Bringing in code from topology project.

--- src/fl_tissue_model_tools/preprocessing.py
+++ src/fl_tissue_model_tools/preprocessing.py
@@ -28,7 +28,6 @@ def gen_circ_mask(center: npt.ArrayLike, rad: float, shape: npt.ArrayLike, mask_
     return cv2.circle(circ_mask, center, rad, mask_val, cv2.FILLED)
 
 
-
 def apply_mask(img: npt.NDArray, mask: npt.NDArray) -> npt.NDArray:
     '''
         Apply the mask {mask} to an image {img}.
@@ -37,6 +36,16 @@ def apply_mask(img: npt.NDArray, mask: npt.NDArray) -> npt.NDArray:
         the same.
     '''
     return cv2.bitwise_and(img, img, mask=mask)
+
+
+def bin_thresh(img: npt.NDArray, img_max: npt.NDArray, threshold: float=0) -> npt.NDArray:
+    '''
+        Threshold the image {img} by setting all pixels
+        in {img} with value above 0 to {img_max}
+    '''
+    img = np.copy(img)
+    img = np.where(img>threshold, img_max, 0)
+    return img
 
 
 def exec_threshold(masked: npt.NDArray, pinhole_idx: npt.ArrayLike, sd_coef: float, rs: RandomState) -> npt.NDArray:
@@ -72,4 +81,53 @@ def exec_threshold(masked: npt.NDArray, pinhole_idx: npt.ArrayLike, sd_coef: flo
     # rescale the foreground to the range [0,1]
     # gmm_masked = defs.GS_MAX*(gmm_masked - fg_thresh)/(defs.GS_MAX - fg_thresh)
     return gmm_masked
+
+
+def dt_blur(img: npt.NDArray, blur_itr: int) -> npt.NDArray[np.uint8]:
+    '''
+        Apply distance transformation and blur the image for {blur_itr} iterations
+    '''
+    # k_size = (25 // ds_fact) + (1 - (25 // ds_fact) % 2)
+
+    # img = cv2.distanceTransform(img.round().astype(np.uint8), cv2.DIST_L2, 5).round().astype(np.uint8)
+    proc_img = cv2.distanceTransform(bin_thresh(img, defs.GS_MAX).round().astype(np.uint8), cv2.DIST_L2, 5)
+    k_size = 3
+    for i in range(blur_itr):
+        proc_img = cv2.GaussianBlur(proc_img, (k_size, k_size), 0)
+    # img = cv2.distanceTransform(img.round().astype(np.uint8), cv2.DIST_L2, 5).round().astype(np.uint8)
+    return proc_img.round().astype(np.uint8)
+
+
+def sdt_blur(img: npt.NDArray, blur_itr: int) -> npt.NDArray[np.uint8]:
+    '''
+        Apply the signed distance transform to a binary image
+        and blur the image for {blur_itr} iterations
+
+        The signed distance transform assigns each pixel in a binary mask
+        its distance to the boundary and assigns each pixel not in the mask
+        -1 times its distance to the boundary
+    '''
+    # all pixels of value greater than 0
+    mask = bin_thresh(img, defs.GS_MAX).round().astype(np.uint8)
+    # distance of pixels in the mask
+    proc_img = cv2.distanceTransform(mask, cv2.DIST_L2, 5)
+    # distance of pixels not in the mask
+    proc_img = proc_img - cv2.distanceTransform(np.logical_not(mask).astype(np.uint8), cv2.DIST_L2, 5)
+
+    k_size = 3
+    for i in range(blur_itr):
+        proc_img = cv2.GaussianBlur(proc_img, (k_size, k_size), 0)
+
+    return proc_img.round()
+
+
+def blur(img: npt.NDArray, blur_itr: int) -> npt.NDArray[np.uint8]:
+    '''
+        Blur the image {img} for {blur_itr} iterations
+    '''
+    proc_img = img
+    k_size = 3
+    for i in range(blur_itr):
+        proc_img = cv2.GaussianBlur(proc_img, (k_size, k_size), 0)
+    return proc_img.round().astype(np.uint8)
 
