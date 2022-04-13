a32f28f42bb664967614477b811c0c3a19db795f
cjc77 <carsonjamescook@gmail.com>
2022-04-12 20:14:41 -0700

    Helper function updated and data generation updated to include weights.

--- src/fl_tissue_model_tools/models.py
+++ src/fl_tissue_model_tools/models.py
@@ -1,8 +1,8 @@
-from ast import Global
+from ast import Global, Mod
 from copy import deepcopy
 from typing import Sequence, Union, Callable, Any
 from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Softmax
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Softmax, Conv2D, BatchNormalization, Activation, SeparableConv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, add
 from tensorflow.keras.applications import resnet50
 from tensorflow.keras.callbacks import History
 from tensorflow.keras.losses import BinaryCrossentropy, Loss
@@ -10,6 +10,20 @@ from tensorflow.keras.metrics import BinaryAccuracy
 from tensorflow.keras.optimizers import Adam, Optimizer
 from tensorflow.keras.activations import sigmoid
 import keras_tuner as kt
+
+
+def _check_consec_factor(x, factor, reverse=False):
+    res = True
+    if reverse:
+        x = list(reversed(x))
+    for i in range(1, len(x)):
+        res = res and (x[i] == x[i - 1] * factor)
+    return res
+    
+
+def toggle_TL_freeze(tl_model: Model, base_model_name: str="base_model") -> None:
+    base_model = tl_model.get_layer(base_model_name)
+    base_model.trainable = not base_model.trainable
 
 
 def build_ResNet50_TL(n_outputs: int, img_shape: tuple[int, int], base_init_weights: str="imagenet", base_last_layer: str="conv5_block3_out", output_act: str="sigmoid", base_model_trainable: bool=False, base_model_name: str="base_model") -> Model:
@@ -31,13 +45,61 @@ def build_ResNet50_TL(n_outputs: int, img_shape: tuple[int, int], base_init_weig
     return model
 
 
-def build_UNetXception(n_outputs: int, img_shape: tuple[int, int]) -> Model:
-    pass
+def build_UNetXception(n_outputs: int, img_shape: tuple[int, int], channels: int=1, filter_counts: tuple[int, int, int, int]=(32, 64, 128, 256), output_act: str="sigmoid") -> Model:
+    inputs = Input(img_shape + (channels,))
+    # Validate filter counts
+    filter_counts = sorted(filter_counts)
+    assert _check_consec_factor(filter_counts, factor=2), f"Filter depths do not increase consecutively by a factor of 2."
 
+    # Downsampling
+    x = Conv2D(filter_counts[0], 3, strides=2, padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
 
-def toggle_TL_freeze(tl_model: Model, base_model_name: str="base_model") -> None:
-    base_model = tl_model.get_layer(base_model_name)
-    base_model.trainable = not base_model.trainable
+    previous_block_activation = x
+
+    # Downsampling Xception blocks
+    for filters in filter_counts[1:]:
+        x = Activation("relu")(x)
+        x = SeparableConv2D(filters, 3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        x = Activation("relu")(x)
+        x = SeparableConv2D(filters, 3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        x = MaxPooling2D(3, strides=2, padding="same")(x)
+        
+        residual = Conv2D(filters, 1, strides=2, padding="same")(previous_block_activation)
+
+        x = add([x, residual])
+
+        previous_block_activation = x
+    
+    # Upsampling
+    for filters in list(reversed(filter_counts)):
+        x = Activation("relu")(x)
+        x = Conv2DTranspose(filters, 3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        x = Activation("relu")(x)
+        x = Conv2DTranspose(filters, 3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        x = UpSampling2D(2)(x)
+
+        residual = UpSampling2D(2)(previous_block_activation)
+        residual = Conv2D(filters, 1, padding="same")(residual)
+        x = add([x, residual])
+        previous_block_activation = x
+    
+    # Output
+    outputs = Conv2D(n_outputs, 3, activation=output_act, padding="same")(x)
+
+    # Define model
+    model = Model(inputs, outputs)
+
+    return model
 
 
 class ResNet50TLHyperModel(kt.HyperModel):
@@ -78,3 +140,7 @@ class ResNet50TLHyperModel(kt.HyperModel):
         toggle_TL_freeze(model, self.base_model_name)
         model.compile(self.fine_tune_optimizer(fine_tune_lr), self.loss, self.metrics)
         return model.fit(*args, **kwargs, epochs=self.fine_tune_epochs)
+
+
+class UNetXceptionHyperModel(kt.HyperModel):
+    pass
