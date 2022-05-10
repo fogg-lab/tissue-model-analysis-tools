09a9f84dfc0fba35b47cd940eb25f94bf6e1a808
cjc77 <carsonjamescook@gmail.com>
2022-05-09 19:21:19 -0700

    Z projection script rough draft (need to do error checking and verbose output).

--- src/fl_tissue_model_tools/script_util.py
+++ src/fl_tissue_model_tools/script_util.py
@@ -2,11 +2,12 @@ import argparse
 import os
 import shutil
 import json
+import re
 from glob import glob
 from dataclasses import dataclass
 from typing import Sequence, Any
 
-from . import defs, data_prep
+from . import defs, data_prep, zstacks
 
 
 @dataclass
@@ -30,7 +31,7 @@ def verbose_header(title: str) -> None:
 
 
 def verbose_footer() -> None:
-    pass
+    print(verbose_end)
 
 
 ### Parse Arguments ###
@@ -58,9 +59,17 @@ def parse_cell_area_args(arg_defaults: dict[str, Any]) -> argparse.Namespace:
 def parse_zproj_args(arg_defaults: dict[str, Any]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("in_root", type=str, help="Full path to root directory of input zstacks. Ex: [...]/my_data/images/experiment_1_yyyy_mm_dd/")
+    parser.add_argument("in_root", type=str, help="Full path to root directory of input zstacks. Ex: [...]/my_data/z_stacks/experiment_1_yyyy_mm_dd/")
 
-    parser.add_argument("out_root", type=str, help="Full path to root directory where output will be stored. Ex: [...]/my_data/analysis_output/experiment_1_yyyy_mm_dd/. In this example, experiment_1_yyyy_mm_dd/ will be created if it does not already exist.")
+    parser.add_argument("out_root", type=str, help="Full path to root directory where output will be stored. Ex: [...]/my_data/z_projections/experiment_1_yyyy_mm_dd/. In this example, experiment_1_yyyy_mm_dd/ will be created if it does not already exist.")
+
+    parser.add_argument("-e", "--extension", type=str, default="tif", help="File extension type of images. If no argument supplied, defaults to tif.")
+
+    parser.add_argument("-m", "--method", type=str, default="fs", choices=["min", "max", "med", "avg", "fs"], help="Z projection method. If no argument supplied, defaults to 'fs' (focus stacking).")
+
+    parser.add_argument("-o", "--order", type=int, default=1, choices=[0, 1], help=f"Interpretation of Z stack order. 0=Ascending, 1=Descending. For Z stack of size k: -o 0 means Z0 is bottom of well while -o 1 means Z0 is top of well. If no argument supplied, defaults to 1=Descending.")
+
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output during script execution.")
 
     args = parser.parse_args()
     return args
@@ -122,3 +131,47 @@ def cell_area_verify_config_file(path: str, verbose: bool=False):
         print(verbose_end)
     return config
 
+
+def zproj_verify_input_dir(path: str, extension: str, verbose: bool=False) -> Sequence[str]:
+    if verbose:
+        verbose_header("Verifying Input Directory")
+    if not os.path.isdir(path):
+        raise FileNotFoundError(f"Input data directory not found:{os.linesep}\t{path}")
+    zstack_paths = [fp.replace("\\", "/") for fp in glob(f"{path}/*")]
+    # zs_counts = {}
+    if verbose:
+        print(f"{'Z Stack ID':<40}{'Image Count':>20}")
+    for zsp in zstack_paths:
+        img_paths = [fp.replace("\\", "/") for fp in glob(f"{zsp}/*.{extension}")]
+        n_imgs = len(img_paths)
+        if n_imgs == 0:
+            raise FileNotFoundError(f"Input data directory contains Z stack subdirectory holding no files with extension:{os.linesep}\t{extension}{os.linesep}Offending subdirectory:{os.linesep}\t{zsp}")
+        for ip in img_paths:
+            iname = ip.split("/")[-1]
+            pattern = re.search(zstacks._zpos_pattern, ip)
+            if pattern is None:
+                raise FileNotFoundError(f"Image file{os.linesep}\t{iname}{os.linesep}does not contain the expected pattern to denote Z position. Files must have ...Z[pos]_... in their name, where [pos] is a number denoting Z stack position.")
+        if verbose:
+            zsp_id = zsp.split("/")[-1]
+            print(f"{zsp_id:.<40}{n_imgs:.>20}")
+            
+    if verbose:
+        print(SFM.success)
+        verbose_footer()
+    return zstack_paths
+
+
+def zproj_verify_output_dir(path: str, verbose=True) -> None:
+    if verbose:
+        verbose_header("Verifying Output Directory")
+    if not os.path.isdir(path):
+        if verbose:
+            print(f"Did not find output dir:{os.linesep}\t{path}")
+            print(f"Creating...")
+        data_prep.make_dir(path)
+        if verbose:
+            print(f"... Created dir:{os.linesep}\t{path}")
+
+    if verbose:
+        print(SFM.success)
+        verbose_footer()
