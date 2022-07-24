fc08bdff1deeb00231bd51460d9e619697dfb4c5
Noah <wigginno@oregonstate.edu>
2022-07-24 03:18:02 -0400

    Added --area arg to compute_zproj.py
    Adding the --area (or -a) flag computes the cell area after computing z projections

--- scripts/compute_zproj.py
+++ scripts/compute_zproj.py
@@ -5,13 +5,15 @@ import numpy as np
 import numpy.typing as npt
 import cv2
 
+import compute_cell_area as area
+
 from fl_tissue_model_tools import defs
 from fl_tissue_model_tools import preprocessing as prep
 from fl_tissue_model_tools import script_util as su
 from fl_tissue_model_tools import zstacks as zs
 
 
-proj_method = {
+proj_methods = {
     "min": zs.proj_min,
     "max": zs.proj_max,
     "med": zs.proj_med,
@@ -35,21 +37,20 @@ def get_zstack(zs_path: str, extension: str, descending: bool) -> npt.NDArray:
 
     Returns:
         Z stack as array of images.
-
     """
-    a = defs.GS_MIN
-    b = defs.GS_MAX
+    new_min = defs.GS_MIN
+    new_max = defs.GS_MAX
 
     if extension == "png":
-        mn = a
-        mx = b
+        old_min = new_min
+        old_max = new_max
     elif extension == "tif":
-        mn = defs.TIF_MIN
-        mx = defs.TIF_MAX
+        old_min = defs.TIF_MIN
+        old_max = defs.TIF_MAX
     else:
         raise OSError(f"Unsupported file type for analysis: {extension}")
-    zstack = zs.zstack_from_dir(zs_path, extension, descending)
-    zstack = prep.min_max_(zstack, a, b, mn, mx)
+    zstack = zs.zstack_from_dir(zs_path, extension, descending)[1]
+    zstack = prep.min_max_(zstack, new_min, new_max, old_min, old_max)
     return zstack
 
 
@@ -68,30 +69,29 @@ def save_zproj(
 
     Raises:
         OSError: Unsupported file extension supplied.
-
     """
-    mn = defs.GS_MIN
-    mx = defs.GS_MAX
+    old_min = defs.GS_MIN
+    old_max = defs.GS_MAX
     if extension == "tif":
         cast_type = np.uint16
-        a = defs.TIF_MIN
-        b = defs.TIF_MAX
+        new_min = defs.TIF_MIN
+        new_max = defs.TIF_MAX
     elif extension == "png":
         cast_type = np.uint8
-        a = mn
-        b = mx
+        new_min = old_min
+        new_max = old_max
     else:
         raise OSError(f"Unsupported file type for analysis: {extension}")
     cv2.imwrite(
         f"{out_root}/{zid}_{zproj_type}.{extension}",
-        prep.min_max_(zproj, a, b, mn, mx).astype(cast_type)
+        prep.min_max_(zproj, new_min, new_max, old_min, old_max).astype(cast_type)
     )
 
 
 def main():
     args = su.parse_zproj_args()
     verbose = args.verbose
-    compute_cell_area = args.compute_cell_area
+    compute_cell_area = args.area
 
     ### Tidy up paths ###
     in_root = args.in_root.replace("\\", "/")
@@ -102,15 +102,15 @@ def main():
     try:
         zstack_paths = su.zproj_verify_input_dir(in_root, extension,
                                                  verbose=verbose)
-    except FileNotFoundError as e:
-        print(f"{su.SFM.failure} {e}")
+    except FileNotFoundError as error:
+        print(f"{su.SFM.failure} {error}")
         sys.exit()
 
     ### Verify output destination ###
     try:
         su.zproj_verify_output_dir(out_root, verbose=verbose)
-    except PermissionError as e:
-        print(f"{su.SFM.failure} {e}")
+    except PermissionError as error:
+        print(f"{su.SFM.failure} {error}")
         sys.exit()
 
     ### Compute Z projections ###
@@ -127,20 +127,20 @@ def main():
             d.delayed({z_ids[i]: get_zstack(zsp, extension, descending)
                         for i, zsp in enumerate(zstack_paths)})
         )[0]
-    except OSError as e:
-        print(f"{su.SFM.failure}{e}")
+    except OSError as error:
+        print(f"{su.SFM.failure}{error}")
         sys.exit()
 
     if verbose:
         print("... Z stacks loaded.")
 
-    pm = proj_method[zp_method]
+    proj_method = proj_methods[zp_method]
 
     if verbose:
         print(f"{os.linesep}Computing projections...")
 
     zprojs = d.compute(
-        d.delayed({z_id: pm(zstacks[z_id]) for z_id in z_ids})
+        d.delayed({z_id: proj_method(zstacks[z_id]) for z_id in z_ids})
     )[0]
     if verbose:
         print("... Projections computed.")
@@ -155,6 +155,15 @@ def main():
         print("... Projections saved.")
         print(su.SFM.success)
         print(su.verbose_end)
+
+    if compute_cell_area:
+        if "-a" in sys.argv:
+            sys.argv.remove("-a")
+        elif "--area" in sys.argv:
+            sys.argv.remove("--area")
+        # Set in_root to out_root (where z projections are saved)
+        sys.argv[-2] = sys.argv[-1]
+        area.main()
 
 
 if __name__ == "__main__":
