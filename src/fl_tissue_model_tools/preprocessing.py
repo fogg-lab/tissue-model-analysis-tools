bc2bb7ed366f80398461c50fd8cbbc19ca8adf9e
noah <wigginno@oregonstate.edu>
2022-09-08 23:47:12 -0700

    merged with main

--- src/fl_tissue_model_tools/preprocessing.py
+++ src/fl_tissue_model_tools/preprocessing.py
@@ -15,14 +15,12 @@ def min_max_(
 ) -> npt.NDArray[np.float64]:
     """Normalize the array `img` from the range [`old_min`, `old_max`]
        to the range [`new_min`, `new_max`]
-
     Args:
         img: The array to be normalized.
         new_min: The new min value.
         new_max: The new max value.
         old_min: The old min value.
         old_max: The old max value.
-
     Returns:
         The normalized array.
     """
@@ -35,20 +33,16 @@ def gen_circ_mask(
     center: Tuple[int, int], rad: float, shape: Tuple[int, int], mask_val: np.uint8
 ) -> npt.NDArray[np.uint8]:
     """Generate a 2D circular mask.
-
     The circle mask is a size `shape` array of uint8, where an element
     has value `mask_val` if it is in the circle and is 0 otherwise.
     The circle is centered at the pixel `center` and has radius `rad`.
-
     Args:
         center: Coordinates of the center pixel of the circle.
         rad: Radius of the circle.
         shape: The shape, (H,W), of the 2D array.
         mask_val: The value to give pixels within the circle.
-
     Returns:
         The masked image.
-
     """
     circ_mask = np.zeros(shape, dtype="uint8")
     return cv2.circle(circ_mask, center, rad, mask_val, cv2.FILLED)
@@ -56,14 +50,11 @@ def gen_circ_mask(
 
 def apply_mask(img: npt.NDArray, mask: npt.NDArray) -> npt.NDArray:
     """Apply a mask to an image.
-
     Args:
         img: Image to be masked.
         mask: Mask to be applied.
-
     Returns:
         Masked image.
-
     """
     return cv2.bitwise_and(img, img, mask=mask)
 
@@ -73,16 +64,13 @@ def bin_thresh(
 ) -> npt.NDArray:
     """Threshold an image by setting all pixels with value above `threshold` to `img_max`
     and all other pixels to 0.
-
     Args:
         img: Image to be thresholded.
         img_max: Value to replace `True`.
         threshold: Min value for which `img_max` is substituted.
-
     Returns:
         Copy of original array with `img_max` replacing pixels
         with values > `threshold` and all other pixels set to 0.
-
     """
     img = np.copy(img)
     img = np.where(img>threshold, img_max, 0)
@@ -93,14 +81,12 @@ def exec_threshold(
     masked: npt.NDArray, mask_idx: Sequence, sd_coef: float, rand_state: RandomState
 ) -> npt.NDArray:
     """Apply threshold to obtain the foreground (cell content) of a plate image.
-
     A 2-component Gaussian mixture model is fit to pixel the intensities of
     a masked image. Intent is that one Gaussian fits background pixels and
     the other fits foreground pixels. Foreground Gaussian has the larger mean.
     Foreground pixels are defined as all pixels with intensities greater than the
     mean of the foreground Gaussian plus `sd_coef` times the standard deviation
     of the foreground Gaussian.
-
     Args:
         masked: Masked image.
         mask_idx: Indices of pixels within the mask boundary.
@@ -109,7 +95,6 @@ def exec_threshold(
             means that intensities to the left of the
             foreground Gaussian's mean will be retained.
         rand_state: A NumPy random state object to allow for reproducability.
-
     Returns:
         Copy of original image with background pixels set to 0.
     """
@@ -133,14 +118,12 @@ def dt_blur(
     img: npt.NDArray, blur_itr: int, dist_metric: int=cv2.DIST_L2, k_size: int=3
 ) -> npt.NDArray[np.uint8]:
     """Apply distance transform and blur the image for `blur_itr` iterations.
-
     Args:
         img: Image to be transformed.
         blur_itr: Number of iterations to apply Gaussian blur.
         dist_metric: Distance computation to use in distance transform.
             Defaults to Euclidean.
         k_size: Kernel size for iterative Gaussian blur.
-
     Returns:
         Distance transformed and blurred image in grayscale format.
     """
@@ -155,21 +138,17 @@ def sdt_blur(
     img: npt.NDArray, blur_itr: int, dist_metric: int=cv2.DIST_L2, k_size: int=3
 ) -> npt.NDArray[np.uint8]:
     """Apply the signed distance and blur the image for `blur_itr` iterations.
-
     The signed distance transform assigns each pixel in a binary mask
     its distance to the boundary and assigns each pixel not in the mask
     -1 times its distance to the boundary
-
     Args:
         img: Image to be transformed.
         blur_itr: Number of iterations to apply Gaussian blur.
         dist_metric: Distance computation to use in distance transform.
             Defaults to Euclidean.
         k_size: Kernel size for iterative Gaussian blur.
-
     Returns:
         Signed distance transformed and blurred image.
-
     """
     # all pixels of value greater than 0
     mask = bin_thresh(img, defs.GS_MAX).round().astype(np.uint8)
@@ -185,16 +164,13 @@ def blur(
     img: npt.NDArray, blur_itr: int, k_size: int=3, gs: bool=True
 ) -> npt.NDArray[np.uint8]:
     """Blur an image for `blur_itr` iterations.
-
     Args:
         img: Image to be transformed.
         blur_itr: Number of iterations to apply Gaussian blur.
         k_size: Kernel size for iterative Gaussian blur.
         gs: Whether to convert resulting image to grayscale.
-
     Returns:
         Blurred image.
-
     """
     proc_img = img.copy()
     for i in range(blur_itr):
@@ -210,7 +186,6 @@ def augment_img(
     vflip: bool, expand_dims: bool=True
 ) -> npt.NDArray[Union[np.float_, np.int_]]:
     """Augment an image using rotations and horizontal/vertical flips
-
     Args:
         img: Original image.
         rot: Rotation angle for image.
@@ -218,7 +193,6 @@ def augment_img(
         vflip: Whether to vertically flip image.
         expand_dims: Whether to add a depth axis to the image after
             augmentation steps.
-
     Returns:
         Augmented image.
     """
@@ -245,12 +219,10 @@ def augment_img_mask_pairs(
     rand_state: RandomState
 ) -> Tuple[npt.NDArray[np.float_], npt.NDArray[np.int_]]:
     """Augment a set of image/mask pairs using rotations and horizontal/vertical flips
-
     Args:
         original_imgs: Original images.
         original_masks: Original masks.
         rand_state: RandomState object to allow for reproducability.
-
     Returns:
         (Augmented images, matched augmented masks)
     """
@@ -281,14 +253,12 @@ def augment_imgs(
     expand_dims: bool=False
 ) -> npt.NDArray[np.float_]:
     """Augment a set of images using rotations and horizontal/vertical flips.
-
     Args:
         x: Original images.
         rand_state: RandomState object to allow for reproducability.
         rot_options: Random rotation angle choices.
         expand_dims: Whether to add a depth axis to each image after
             augmentation steps.
-
     Returns:
         Augmented image set.
     """
@@ -309,14 +279,12 @@ def map2bin(
     fg: int=1, bg: int=0
 ) -> npt.NDArray[np.int_]:
     """Convert a mask n-map (e.g., trimap) to a simple binary map.
-
     Args:
         lab: Label mask.
         fg_vals: Foreground mask values.
         bg_vals: Background mask values.
         fg: Desired mask foreground value.
         bg: Desired mask background value.
-
     Returns:
         Binary mask of same dimension as input mask.
     """
@@ -330,11 +298,9 @@ def map2bin(
 
 def balanced_class_weights_from_counts(class_counts) -> Dict[Any, float]:
     """Create balanced weights using class counts.
-
     Args:
         class_counts: Counts of number of items in each class. Example:
             {c1: n_c1, c2: n_c2, ..., ck: n_ck}.
-
     Returns:
         dict[Any, float]: Weights for each class. Example:
             {c1: w_c1, c2: w_c2, ..., ck: w_ck}.
