ef5796653cec4c591848cd59e556f7632aa51ade
Noah Wiggin <wigginno@oregonstate.edu>
2023-02-17 13:50:26 -0500

    misc code cleanup

--- src/fl_tissue_model_tools/defs.py
+++ src/fl_tissue_model_tools/defs.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 
-TIF_MIN = np.iinfo(np.uint16).min
 TIF_MAX = np.iinfo(np.uint16).max
-GS_MIN = np.iinfo(np.uint8).min
 GS_MAX = np.iinfo(np.uint8).max
 
 SCRIPT_REL_MODEL_TRAINING_PATH = "../model_training"
