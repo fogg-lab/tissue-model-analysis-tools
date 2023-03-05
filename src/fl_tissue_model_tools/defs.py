aee2d7ef0b951fa4dbff228b82466bb401db61c1
Noah Wiggin <wigginno@oregonstate.edu>
2023-03-05 04:40:32 -0500

    finished cli.py entrypoint and configuration script
    Former-commit-id: b9240ff007abcc2e6a3244fcc9b5e528438f96d9

--- src/fl_tissue_model_tools/defs.py
+++ src/fl_tissue_model_tools/defs.py
@@ -6,23 +6,23 @@ import numpy as np
 MAX_UINT16 = np.iinfo(np.uint16).max
 MAX_UINT8 = np.iinfo(np.uint8).max
 
-_PKG_BASE_DIR = Path(__file__).resolve().parent.parent
-_SETUP_CFG = configparser.ConfigParser()
-_SETUP_CFG.read(_PKG_BASE_DIR / 'setup.cfg')
-PKG_NAME = _SETUP_CFG['metadata']['name']
+PKG_BASE_DIR = Path(__file__).resolve().parent.parent
+_setup_cfg = configparser.ConfigParser()
+_setup_cfg.read(PKG_BASE_DIR / 'setup.cfg')
+PKG_NAME = _setup_cfg['metadata']['name']
 
-_PKG_CFG_PATH = pkg_resources.resource_filename(PKG_NAME, f'{PKG_NAME}.cfg')
-_PKG_CONFIG = configparser.ConfigParser()
-_PKG_CONFIG.read(_PKG_CFG_PATH)
+PKG_CFG_PATH = pkg_resources.resource_filename(PKG_NAME, f'{PKG_NAME}.cfg')
+_pkg_config = configparser.ConfigParser()
+_pkg_config.read(PKG_CFG_PATH)
 
-PKG_SCRIPTS_DIR = _PKG_BASE_DIR / 'scripts'
-PKG_CONFIG_DIR = _PKG_BASE_DIR / 'config'
+PKG_SCRIPTS_DIR = PKG_BASE_DIR / 'scripts'
+PKG_CONFIG_DIR = PKG_BASE_DIR / 'config'
 
-BASE_DIR = _PKG_CONFIG[PKG_NAME]['base_dir']
-if BASE_DIR.startswith('~'):
-    BASE_DIR = Path.home().resolve() / BASE_DIR[2:]
+_base_dir = _pkg_config[PKG_NAME]['base_dir']
+if _base_dir.startswith('~'):
+    BASE_DIR = Path.home().resolve() / _base_dir[2:]
 else:
-    BASE_DIR = Path(BASE_DIR)
+    BASE_DIR = Path(_base_dir)
 
 MODEL_TRAINING_DIR = BASE_DIR / 'model_training'
 SCRIPT_CONFIG_DIR = BASE_DIR / 'config'
