33d8c7260bc79b536270e9c32dbb3618b25eca5e
Noah Wiggin <wigginno@oregonstate.edu>
2023-03-06 16:49:13 -0500

    fix bug in update_package.py
    Former-commit-id: ef13b778471fadb9b893b27768d13ccfa3bf0ccb

--- src/fl_tissue_model_tools/update_package.py
+++ src/fl_tissue_model_tools/update_package.py
@@ -8,6 +8,6 @@ SUBDIR = 'src'
 
 def update_package():
     """Update the package from GitHub."""
-    cmd = f'pip install -I \'{PKG_NAME} @ {GITHUB_LINK}#subdirectory={SUBDIR}\''
+    cmd = f'pip install -I {PKG_NAME}@{GITHUB_LINK}#subdirectory={SUBDIR}'
     print(f'Running command: {cmd}')
     subprocess.run(cmd, shell=True, check=True)
