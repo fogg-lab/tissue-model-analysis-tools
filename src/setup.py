808ce99d499f8e84df40abecd8d8fe4fe41233ab
Noah Wiggin <wigginno@oregonstate.edu>
2023-03-05 23:16:42 -0500

    add pydmtgraph to setup.py install

--- src/setup.py
+++ src/setup.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import configparser
 from setuptools import setup, Extension
+from platform import python_version_tuple
 import numpy as np
 
 SETUP_CFG = configparser.ConfigParser()
@@ -31,12 +32,27 @@ config[PKG_NAME] = {
 with open(CFG_FILE, 'w', encoding='utf-8') as config_file:
     config.write(config_file)
 
+# Boost libraries
+py_version = python_version_tuple()
+libraries = [f"boost_python{py_version[0]}{py_version[1]}",
+             f"boost_numpy{py_version[0]}{py_version[1]}"]
+
+# Add lib and include dirs if boost is installed in a conda environment
+env_path = os.environ.get("CONDA_PREFIX")
+if env_path:
+    env_library_paths = (env_path, os.path.join(env_path, "Library"))
+    library_dirs = [os.path.join(p, "lib") for p in env_library_paths]
+    include_dirs = [os.path.join(p, "include") for p in env_library_paths]
+else:
+    library_dirs = None
+    include_dirs = None
+
 setup(
     name=PKG_NAME,
     version='0.1.0',
     author='Fogg Lab',
     packages=[PKG_NAME],
-    package_data={'': [str(CFG_FILE)]},
+    package_data={PKG_NAME: [str(CFG_FILE)]},
     include_package_data=True,
     url='https://github.com/fogg-lab/tissue-model-analysis-tools',
     license='MIT',
@@ -44,12 +60,21 @@ setup(
         'Cython',
         'numpy'
     ],
-    ext_modules=[Extension(
-        name=f'{PKG_NAME}.gwdt_impl',
-        sources=[os.path.join(PKG_NAME, 'gwdt_impl.pyx')],
-        include_dirs=[np.get_include()],
-        language='c++'
-    )],
+    ext_modules=[
+        Extension(
+            name=f'{PKG_NAME}.gwdt_impl',
+            sources=[os.path.join(PKG_NAME, 'gwdt_impl.pyx')],
+            include_dirs=[np.get_include()],
+            language='c++'
+        ),
+        Extension(
+            name="pydmtgraph.dmtgraph",
+            sources=["pydmtgraph/src/pydmtgraph/dmtgraph/DMTGraph.cpp"],
+            library_dirs=library_dirs,
+            include_dirs=include_dirs,
+            extra_compile_args=["--std=c++11"]
+        )
+    ],
     entry_points={
         'console_scripts': [f'{command}={ENTRYPOINT_FUNCTION}' for command in ENTRYPOINT_COMMANDS]
     }
